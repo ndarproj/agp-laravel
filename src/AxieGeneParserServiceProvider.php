60893d4918f0a17a2002f6eb7bd00452d2439c23
ndarproj <ndarproj@gmail.com>
2022-10-10 02:54:15 +0800

    first commit

---
+++ src/AxieGeneParserServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ndarproj\AxieGeneParser;
+
+use Illuminate\Support\ServiceProvider;
+
+class AxieGeneParserServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+    public function register()
+    {
+    }
+}
